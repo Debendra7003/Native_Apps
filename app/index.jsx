1f2b545bc5bd49ea334012f1e0b56a008270b2db
DEBENDRA <161910591+Debendra7003@users.noreply.github.com>
2025-09-26 15:22:18 +0530

    push

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Text,View } from "react-native";
+
+const Index = () => {
+  return (
+    <View style={{ 
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      backgroundColor: '#fa9930ff' 
+      }}>
+      <Text style={{fontSize:30, fontWeight:800}}>Hello world!</Text>
+    </View>
+  );
+};
+export default Index;
